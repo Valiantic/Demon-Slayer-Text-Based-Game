06079158eb611d336fc7e9d58c97ff8fdbc25d1f
StevenMadali <stevenmadali17@gmail.com>
2024-06-23 22:12:04 +0800

    Added mission location

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-
+import random 
 
 def Start():
     print("\n\n----------Kimetsu No Python: Code of the Demon Slayers----------")
     print("------------------Developed by Steven Madali--------------------\n\n\n\n")
     button = input(">>>>Press [K] to Hunt<<<<\n\n>>>>Press [X] to Exit<<<<\n")  # Get user input
 
-    if button == "k":
+    if button == "k" or "K":
         Characterselect()
     else:
         exit()
@@ -15,9 +15,9 @@ def Characterselect():
     while True:
         print("------------------Pick Your Demon Slayer--------------------")
         print("[1] Aoi Kanzaki\n The Flame Breathing Prodigy\n -Known 4 Flame Breathing Forms\n")
-        print("[2] Hiro Aoiumi\n The Water Breathing Prodigy\n -Known 7 Water Breathing Forms\n")
-        print("[3] Kasumi Arashi\n The Wind Breathing Successor\n -Known 5 Wind Breathing Forms\n")
-        print("[4] Kaito Kaminari\n The Thunder Breathing Prodigy\n -Known 3 Thunder Breathing Forms\n")
+        print("[2] Hiro Aoiumi\n The Water Breathing Successor\n -Known 7 Water Breathing Forms\n")
+        print("[3] Kasumi Arashi\n The Wind Breathing Jackal\n -Known 5 Wind Breathing Forms\n")
+        print("[4] Kaito Kaminari\n The Thunder Breathing Genius\n -Known 3 Thunder Breathing Forms\n")
         selectcharacter = int(input())
         
         if selectcharacter == 1:
@@ -34,15 +34,97 @@ def Characterselect():
             break
         else:
             print("Please choose an active Demon Slayer!")
+                 
+# CHARACTERS
 
 def Aoi_Kenzaki():
-    print("you choose aoi")
+    print("You Choose Aoi Kanzaki\nThe Flame Breathing Prodigy\n")
+    mission_place = ["Mount Yokogawa", "Swordsmith Village", "Entertainment District", "Crimson Light District"]
+    
+    random_mission = random.choice(mission_place)
+    
+    print(">You Mission is to go in: \n", random_mission,"!\n")
+    if random_mission == "Mount Yokogawa":
+        input("Press anything to Enter\n")
+        Mount_Yokogawa()
+    elif random_mission == "Swordsmith Village":
+        input("Press anything to Enter\n")
+        Swordsmith_Village()
+    elif random_mission == "Entertainment District":
+        input("Press anything to Enter\n")
+        Entertainment_District()
+    elif random_mission == "Crimson Light District":
+        input("Press anything to Enter\n")
+        Crimson_Light_District()
+        
 def Hiro_Aoiumi():
-    pass
+    print("You Choose Hiro Aoiumi\nThe Water Breathing Prodigy\n")
+    mission_place = ["Mount Yokogawa", "Swordsmith Village", "Entertainment District", "Crimson Light District"]
+    
+    random_mission = random.choice(mission_place)
+    
+    print(">You Mission is to go in: \n", random_mission,"!\n")
+    if random_mission == "Mount Yokogawa":
+        input("Press anything to Enter\n")
+        Mount_Yokogawa()
+    elif random_mission == "Swordsmith Village":
+        input("Press anything to Enter\n")
+        Swordsmith_Village()
+    elif random_mission == "Entertainment District":
+        input("Press anything to Enter\n")
+        Entertainment_District()
+    elif random_mission == "Crimson Light District":
+        input("Press anything to Enter\n")
+        Crimson_Light_District()
+        
 def Kasumi_Arashi():
-    pass
+    print("You Choose Kasumi Arashi\nThe Wind Breathing Jackal\n")
+    mission_place = ["Mount Yokogawa", "Swordsmith Village", "Entertainment District", "Crimson Light District"]
+    
+    random_mission = random.choice(mission_place)
+    
+    print(">You Mission is to go in: \n", random_mission,"!\n")
+    if random_mission == "Mount Yokogawa":
+        input("Press anything to Enter\n")
+        Mount_Yokogawa()
+    elif random_mission == "Swordsmith Village":
+        input("Press anything to Enter\n")
+        Swordsmith_Village()
+    elif random_mission == "Entertainment District":
+        input("Press anything to Enter\n")
+        Entertainment_District()
+    elif random_mission == "Crimson Light District":
+        input("Press anything to Enter\n")
+        Crimson_Light_District()
+        
 def Kaito_Kaminari():
-    pass
+    print("You Choose Kaito Kaminari\nThe Thunder Breathing Genius\n")
+    mission_place = ["Mount Yokogawa", "Swordsmith Village", "Entertainment District", "Crimson Light District"]
+    
+    random_mission = random.choice(mission_place)
+    
+    print(">You Mission is to go in: \n", random_mission,"!\n")
+    if random_mission == "Mount Yokogawa":
+        input("Press anything to Enter\n")
+        Mount_Yokogawa()
+    elif random_mission == "Swordsmith Village":
+        input("Press anything to Enter\n")
+        Swordsmith_Village()
+    elif random_mission == "Entertainment District":
+        input("Press anything to Enter\n")
+        Entertainment_District()
+    elif random_mission == "Crimson Light District":
+        input("Press anything to Enter\n")
+        Crimson_Light_District()
+    
+def Mount_Yokogawa():
+    print("Welcome to Mount Yokogawa!")
+def Swordsmith_Village():
+    print("Welcome to Swordsmith Village!")
+def Entertainment_District():
+    print("Welcome to Entertainment District!")
+def Crimson_Light_District():
+    print("Welcome to Crimson Light District!")
 
 if __name__ == '__main__':
     Start()
