3e69e072f3cf08c2c58fd7b2d8cefbe263f3bf08
StevenMadali <stevenmadali17@gmail.com>
2024-06-23 19:03:48 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+
+
+def Start():
+    print("\n\n----------Kimetsu No Python: Code of the Demon Slayers----------")
+    print("----------Developed by Steven Madali----------\n\n\n\n")
+    button = input(">>>>Press [K] to Hunt<<<<\n\n>>>>Press [X] to Exit<<<<\n")  # Get user input
+
+    if button == "k":
+        Characterselect()
+    else:
+        exit()
+
+def Characterselect():
+    print("\nWelcome player")
+
+if __name__ == '__main__':
+    Start()
